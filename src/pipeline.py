5517d6b074441ca845b4ef0a70eb57e941189242
Jatin <jatin.km@gmail.com>
2024-02-25 20:31:55 +0530

    Added compilation step after loading the model

--- src/pipeline.py
+++ src/pipeline.py
@@ -7,36 +7,37 @@ from config.constants import Constants
 import tensorflow as tf
 
 class PipelineHandler:
-	def __init__(self, TRAINING_DATA_ROOT, SAMPLE_RATE, SEGMENT_LENGTH_IN_SECONDS, FRAME_SIZE, HOP_LENGTH, NUMBER_OF_OUTPUT_CHANNELS = 2, PREDICTION_DATA_ROOT = None):
-		self.config: ConfigurationHandler = ConfigurationHandler(TRAINING_DATA_ROOT, SAMPLE_RATE, SEGMENT_LENGTH_IN_SECONDS, FRAME_SIZE, HOP_LENGTH, NUMBER_OF_OUTPUT_CHANNELS , PREDICTION_DATA_ROOT)
-		self.datasetHandler: DatasetHandler = None 
-		self.unetModel: UNET = None
-		self.predictionDatasetHandler: DatasetHandler = None 
+    defaultWeightDecay = 1e-6
+    defaultLearningRate = 1e-3
 
-		self.trainingDataset: tf.data.Dataset = None
-		self.testDataset: tf.data.Dataset = None
+    def __init__(self, TRAINING_DATA_ROOT, SAMPLE_RATE, SEGMENT_LENGTH_IN_SECONDS, FRAME_SIZE, HOP_LENGTH, NUMBER_OF_OUTPUT_CHANNELS = 2, PREDICTION_DATA_ROOT = None):
+        self.config: ConfigurationHandler = ConfigurationHandler(TRAINING_DATA_ROOT, SAMPLE_RATE, SEGMENT_LENGTH_IN_SECONDS, FRAME_SIZE, HOP_LENGTH, NUMBER_OF_OUTPUT_CHANNELS , PREDICTION_DATA_ROOT)
+        self.datasetHandler: DatasetHandler = None 
+        self.unetModel: UNET = None
+        self.predictionDatasetHandler: DatasetHandler = None 
 
-		
-	def preprocess(self, trainingDataRootPath=None):
-		if trainingDataRootPath:
-			self.config.TRAINING_DATA_ROOT = trainingDataRootPath
-        
-		self.datasetHandler = DatasetHandler(self.config)
-		self.trainingDataset, self.testDataset = self.datasetHandler.loadAndPreprocessData(type = Constants.TRAINING_DATA)
+        self.trainingDataset: tf.data.Dataset = None
+        self.testDataset: tf.data.Dataset = None
 
+    def preprocess(self, trainingDataRootPath=None):
+        if trainingDataRootPath:
+            self.config.TRAINING_DATA_ROOT = trainingDataRootPath
 
-	def trainModel(self, weightDecay=1e-6, learningRate = 1e-3):
-		if os.path.exists(Constants.CHECKPOINT_PATH.value): 
-			self.unetModel = tf.keras.models.load_model(Constants.CHECKPOINT_PATH.value)
-		else:
-			self.unetModel = UNET(self.config.INPUT_SHAPE, self.config.NUMBER_OF_OUTPUT_CHANNELS)
-			optimizer = tf.keras.optimizers.AdamW(weight_decay=weightDecay, learning_rate=learningRate)
+        self.datasetHandler = DatasetHandler(self.config)
+        self.trainingDataset, self.testDataset = self.datasetHandler.loadAndPreprocessData(type = Constants.TRAINING_DATA)
 
-			self.unetModel.compile(loss = EvaluationHandler.drumsLossFunction, optimizer = optimizer, metrics=["mse"])
-			
-			learningRateSchedulerCallback = tf.keras.callbacks.LearningRateScheduler(EvaluationHandler.learningRateScheduler)
-			
-			checkpointCallback = tf.keras.callbacks.ModelCheckpoint(
+    def trainModel(self, weightDecay=defaultWeightDecay, learningRate = defaultLearningRate):
+        if os.path.exists(Constants.CHECKPOINT_PATH.value): 
+            self.unetModel = tf.keras.models.load_model(Constants.CHECKPOINT_PATH.value)
+        else:
+            self.unetModel = UNET(self.config.INPUT_SHAPE, self.config.NUMBER_OF_OUTPUT_CHANNELS)
+            optimizer = tf.keras.optimizers.AdamW(weight_decay=weightDecay, learning_rate=learningRate)
+
+            self.unetModel.compile(loss = EvaluationHandler.drumsLossFunction, optimizer = optimizer, metrics=["mse"])
+
+            learningRateSchedulerCallback = tf.keras.callbacks.LearningRateScheduler(EvaluationHandler.learningRateScheduler)
+
+            checkpointCallback = tf.keras.callbacks.ModelCheckpoint(
 				filepath=Constants.CHECKPOINT_PATH.value,
 				save_weights_only=False,
 				save_best_only=True,
@@ -44,9 +45,9 @@ class PipelineHandler:
 				verbose=1,
 			)
 
-			callbacks = [checkpointCallback, learningRateSchedulerCallback]
+            callbacks = [checkpointCallback, learningRateSchedulerCallback]
 
-			self.unetModel.fit(
+            self.unetModel.fit(
 				self.trainingDataset,
 				validation_data = self.testDataset,
 				callbacks=callbacks,
@@ -55,22 +56,34 @@ class PipelineHandler:
 				verbose=1
 			)
 
-		savePath = Constants.CHECKPOINT_PATH.value.split('/')[0]
-		if not os.path.exists(savePath):
-			os.makedirs(savePath)
+        savePath = Constants.CHECKPOINT_PATH.value.split('/')[0]
+        if not os.path.exists(savePath):
+            os.makedirs(savePath)
 
-		self.unetModel.save(Constants.CHECKPOINT_PATH.value)
-    
-    
-	def predict(self, predictionDataPath = None):
-		if predictionDataPath:
-			self.config.PREDICTION_DATA_ROOT = predictionDataPath
-        
-		if os.path.exists(Constants.SONG_TO_SEPERATE_PATH.value):
-			self.predictionDatasetHandler = DatasetHandler(self.config)
-			predictionDataset = self.predictionDatasetHandler.loadAndPreprocessData(type = Constants.PREDICTION_DATA)
-			
-			unetModel = tf.keras.models.load_model(Constants.CHECKPOINT_PATH.value)
-			predictedSpectrograms = unetModel.predict(predictionDataset)
-			
-			self.predictionDatasetHandler.postProcessAndSavePrediction(predictedSpectrograms)
+        self.unetModel.save(Constants.CHECKPOINT_PATH.value)
+
+    def predict(self, predictionDataPath=None):
+        if predictionDataPath:
+            self.config.PREDICTION_DATA_ROOT = predictionDataPath
+
+        if os.path.exists(Constants.SONG_TO_SEPERATE_PATH.value):
+            self.predictionDatasetHandler = DatasetHandler(self.config)
+            predictionDataset = self.predictionDatasetHandler.loadAndPreprocessData(
+                type=Constants.PREDICTION_DATA
+            )
+
+            self.unetModel = tf.keras.models.load_model(Constants.CHECKPOINT_PATH.value)
+            optimizer = tf.keras.optimizers.AdamW(
+                weight_decay=PipelineHandler.defaultWeightDecay,
+                learning_rate=PipelineHandler.defaultLearningRate,
+            )
+            self.unetModel.compile(
+                loss=EvaluationHandler.drumsLossFunction,
+                optimizer=optimizer,
+                metrics=["mse"],
+            )
+            predictedSpectrograms = self.unetModel.predict(predictionDataset)
+
+            self.predictionDatasetHandler.postProcessAndSavePrediction(
+                predictedSpectrograms
+            )
