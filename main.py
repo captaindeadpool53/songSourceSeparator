7da02a3745f39168aaeb8083ee922883bad975cc
Jatin Kumar <53147719+captaindeadpool53@users.noreply.github.com>
2024-03-05 16:17:24 +0000

    change in segment legth

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main(projectDataRootPath, learningRate, alpha, weightDecay, epochs, batchSiz
     pipelineHandler = PipelineHandler(
         FRAME_SIZE=2048,
         HOP_LENGTH=256,
-        SEGMENT_LENGTH_IN_SECONDS=2,
+        SEGMENT_LENGTH_IN_SECONDS=1.5,
         SAMPLE_RATE=22050,  # Using low sample rate due to computational constraints.
         PROJECT_ROOT_PATH = projectDataRootPath,
         BATCH_SIZE = batchSize
