49c585586a40e57989b074add94dce7f751ec988
Jatin <jatin.km@gmail.com>
2024-03-24 15:49:50 +0530

    Restored previous settings

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from src.pipeline import PipelineHandler
 def main(projectDataRootPath, learningRate, alpha, weightDecay, epochs, batchSize):
     pipelineHandler = PipelineHandler(
         FRAME_SIZE=2048,
-        HOP_LENGTH=512,
-        SEGMENT_LENGTH_IN_SECONDS=2,
+        HOP_LENGTH=256,
+        SEGMENT_LENGTH_IN_SECONDS=4,
         SAMPLE_RATE=44100,  # Using low sample rate due to computational constraints.
         PROJECT_ROOT_PATH = projectDataRootPath,
         BATCH_SIZE = batchSize
