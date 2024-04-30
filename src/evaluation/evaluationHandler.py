73ddc3ac8c8776a13a7773f80e61daf3b5a0a065
Jatin Kumar <jatin.kumar@peoplestrong.com>
2024-04-30 22:46:13 +0530

    Code optimizations

--- src/evaluation/evaluationHandler.py
+++ src/evaluation/evaluationHandler.py
@@ -12,61 +12,54 @@ class EvaluationHandler:
     @staticmethod
     def drumsLossFunction5(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.5
-        drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
-        accompanimentTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 1] -target[...,1]))
+        return EvaluationHandler.getLoss(alpha, target, prediction)
 
-        totalLoss = alpha * drumsTrackLoss + (1 - alpha) * accompanimentTrackLoss
-        
-        return totalLoss
     
     @tf.function
     @staticmethod
     def drumsLossFunction1(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =1
-        drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
-        accompanimentTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 1] -target[...,1]))
+        return EvaluationHandler.getLoss(alpha, target, prediction)
 
-        totalLoss = alpha * drumsTrackLoss + (1 - alpha) * accompanimentTrackLoss
-        
-        return totalLoss
     
     @tf.function
     @staticmethod
     def drumsLossFunction0(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0
-        drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
-        accompanimentTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 1] -target[...,1]))
+        return EvaluationHandler.getLoss(alpha, target, prediction)
 
-        totalLoss = alpha * drumsTrackLoss + (1 - alpha) * accompanimentTrackLoss
-        
-        return totalLoss
 
     @tf.function
     @staticmethod
     def drumsLossFunction01(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.1
-        drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
-        accompanimentTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 1] -target[...,1]))
+        return EvaluationHandler.getLoss(alpha, target, prediction)
 
-        totalLoss = alpha * drumsTrackLoss + (1 - alpha) * accompanimentTrackLoss
-        
-        return totalLoss
     
     @tf.function
     @staticmethod
     def drumsLossFunction2(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.2
-        drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
-        accompanimentTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 1] -target[...,1]))
+        return EvaluationHandler.getLoss(alpha, target, prediction)
 
-        totalLoss = alpha * drumsTrackLoss + (1 - alpha) * accompanimentTrackLoss
-        
-        return totalLoss
 
     @tf.function
     @staticmethod
     def drumsLossFunction79(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.7919
+        return EvaluationHandler.getLoss(alpha, target, prediction)
+
+    
+    @tf.function
+    @staticmethod
+    def drumsLossFunction47(target: tf.Tensor,prediction: tf.Tensor) -> float:
+        alpha =0.478
+        return EvaluationHandler.getLoss(alpha, target, prediction)
+
+
+    @tf.function
+    @staticmethod
+    def getLoss(alpha, target, prediction):
         drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
         accompanimentTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 1] -target[...,1]))
 
@@ -74,16 +67,6 @@ class EvaluationHandler:
         
         return totalLoss
     
-    @tf.function
-    @staticmethod
-    def drumsLossFunction47(target: tf.Tensor,prediction: tf.Tensor) -> float:
-        alpha =0.478
-        drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
-        accompanimentTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 1] -target[...,1]))
-
-        totalLoss = alpha * drumsTrackLoss + (1 - alpha) * accompanimentTrackLoss
-        
-        return totalLoss
     
     @staticmethod
     def learningRateScheduler(epoch:int, lr:float)->float:
