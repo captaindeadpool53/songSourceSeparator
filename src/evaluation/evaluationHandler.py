85c82e7d49637f3e603d3274ab4ae81b92bc792a
Jatin Kumar <jatin.kumar@peoplestrong.com>
2024-05-05 18:16:05 +0530

    Added optimisation for tf function

--- src/evaluation/evaluationHandler.py
+++ src/evaluation/evaluationHandler.py
@@ -8,56 +8,56 @@ class EvaluationHandler:
     """
 	Loss function when we have two tracks as output - drums and accompaniments
 	"""
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def drumsLossFunction5(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.5
         return EvaluationHandler.getLoss(alpha, target, prediction)
 
     
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def drumsLossFunction1(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =1
         return EvaluationHandler.getLoss(alpha, target, prediction)
 
     
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def drumsLossFunction0(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0
         return EvaluationHandler.getLoss(alpha, target, prediction)
 
 
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def drumsLossFunction01(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.1
         return EvaluationHandler.getLoss(alpha, target, prediction)
 
     
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def drumsLossFunction2(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.2
         return EvaluationHandler.getLoss(alpha, target, prediction)
 
 
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def drumsLossFunction79(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.7919
         return EvaluationHandler.getLoss(alpha, target, prediction)
 
     
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def drumsLossFunction47(target: tf.Tensor,prediction: tf.Tensor) -> float:
         alpha =0.478
         return EvaluationHandler.getLoss(alpha, target, prediction)
 
 
-    @tf.function
+    @tf.function(reduce_retracing=True)
     @staticmethod
     def getLoss(alpha: float, target: tf.Tensor,prediction: tf.Tensor) -> float:
         drumsTrackLoss = tf.reduce_mean(tf.abs(prediction[..., 0] - target[..., 0]))
